95659eec9708f6a7bbaea964a5e7c42a8619ca55
np1139059565 <1139059565@qq.com>
2022-02-24 22:09:36 +0800

    wx/local_db.js refush_db

--- wx/wx_file.js
+++ wx/wx_file.js
@@ -100,6 +100,45 @@ const f_is_dir=(path) =>{
     }else return null
  }
 
+/**
+ * 
+ * @param {string} dir_path 
+ * @returns {Array[string]} files
+ */
+ const f_readdir=(dir_path)=>FSM.readdirSync(f_to_absolute_path(dir_path))//[p1,p2]
+
+ /**
+  * 	属性	类型	默认值	必填	说明	最低版本
+ 
+ position	number		否	从文件指定位置开始读，如果不指定，则从文件头开始读。读取的范围应该是左闭右开区间 [position, position+length)。有效范围：[0, fileLength - 1]。单位：byte	2.10.0
+ length	number		否	指定文件的长度，如果不指定，则读到文件末尾。有效范围：[1, fileLength]。单位：byte	2.10.0
+ success	function		否	接口调用成功的回调函数	
+ fail	function		否	接口调用失败的回调函数	
+ complete	function		否	接口调用结束的回调函数（调用成功、失败都会执行）	
+ object.success 回调函数
+ 
+  * @param {string} file_path 是	要读取的文件的路径 (本地路径)
+  * @param {string} encoding			否	指定读取文件的字符编码，如果不传 encoding，则以 ArrayBuffer 格式读取文件的二进制内容	
+ 合法值	说明
+ ascii	
+ base64	
+ binary	
+ hex	
+ ucs2	以小端序读取
+ ucs-2	以小端序读取
+ utf16le	以小端序读取
+ utf-16le	以小端序读取
+ utf-8	
+ utf8	
+ latin1	
+  * @returns 
+  */
+ function f_readfile(file_path, encoding="utf-8") {
+     f_info("read file...", file_path,encoding)
+     return f_isexist(file_path)?FSM.readFileSync(f_to_absolute_path(file_path), encoding):null
+ }
+
+
 
 
 
@@ -149,43 +188,7 @@ function f_unzip_sync(zipPath, dstPath, callback) {
     }
 }
 
-/**
- * 
- * @param {string} dir_path 
- * @returns {Array[string]} files
- */
-const f_readdir=(dir_path)=>FSM.readdirSync(dir_path)//[p1,p2]
 
-/**
- * 	属性	类型	默认值	必填	说明	最低版本
-
-position	number		否	从文件指定位置开始读，如果不指定，则从文件头开始读。读取的范围应该是左闭右开区间 [position, position+length)。有效范围：[0, fileLength - 1]。单位：byte	2.10.0
-length	number		否	指定文件的长度，如果不指定，则读到文件末尾。有效范围：[1, fileLength]。单位：byte	2.10.0
-success	function		否	接口调用成功的回调函数	
-fail	function		否	接口调用失败的回调函数	
-complete	function		否	接口调用结束的回调函数（调用成功、失败都会执行）	
-object.success 回调函数
-
- * @param {string} file_path 是	要读取的文件的路径 (本地路径)
- * @param {string} encoding			否	指定读取文件的字符编码，如果不传 encoding，则以 ArrayBuffer 格式读取文件的二进制内容	
-合法值	说明
-ascii	
-base64	
-binary	
-hex	
-ucs2	以小端序读取
-ucs-2	以小端序读取
-utf16le	以小端序读取
-utf-16le	以小端序读取
-utf-8	
-utf8	
-latin1	
- * @returns 
- */
-function f_readfile(file_path, encoding="utf-8") {
-    f_info("read file...", file_path,encoding)
-    return f_isexist(file_path)?FSM.readFileSync(file_path, encoding):null
-}
 
 
 function f_remove_path(path) {
